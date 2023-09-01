79bece37e0c13dbaa30a5fd7a69547723714e6d9
calmw <calm.wang@hotmail.com>
2023-09-01 14:48:57 +0800

    用户定位主网部署

--- scripts/city/intoCityProxy.js
+++ scripts/city/intoCityProxy.js
@@ -2,7 +2,7 @@ const {ethers, upgrades} = require("hardhat")
 require('@openzeppelin/hardhat-upgrades')
 
 // const ProxyAddr = "0x9c189E3989A2c0B6C3b73eA6A77F947cB71b50B1" // 测试网
-const ProxyAddr = "0x9c189E3989A2c0B6C3b73eA6A77F947cB71b50B1" // 主网
+const ProxyAddr = "0xebD06631510A66968f0379A4deB896d3eE7DD6ED" // 主网
 
 // 可升级合约
 async function main() {
@@ -23,8 +23,8 @@ async function main() {
 // logicAddress="0x6eC189557273d7AFF1A12Ab1Bd3b935Cb5eFbf11"
 
 // 主网
-// proxyAdminAddress="0xb6e35b62eC8077538Cb0209Ce744d05E8ea686d9"
-// logicAddress="0x6eC189557273d7AFF1A12Ab1Bd3b935Cb5eFbf11"
+// proxyAdminAddress="0x64A9fa6Fd332d85552E365b6877055D7C92Fa45A"
+// logicAddress="0x21BFE73CB4f3c136a108435855e1420d6844d983"
 
 main()
     .then(() => process.exit(0))
--- scripts/userLocation/intoUserLocationProxy.js
+++ scripts/userLocation/intoUserLocationProxy.js
@@ -2,7 +2,7 @@ const {ethers, upgrades} = require("hardhat")
 require('@openzeppelin/hardhat-upgrades')
 
 // const ProxyAddress = "0x694D23cDA706eC8816C9598A523005cb4410Bf21" // 测试网
-const ProxyAddress = "0x694D23cDA706eC8816C9598A523005cb4410Bf21"
+const ProxyAddress = "0x1B535f616B0465891Bc0bb71307A8781A8cCB8f2"
 
 // 可升级合约
 async function main() {
@@ -23,8 +23,8 @@ async function main() {
 // logicAddress="0x17435E20dDd57A6Ff5374B6e85CCAEfcE2422f27"
 
 // 主网
-// proxyAdminAddress="0xb6e35b62eC8077538Cb0209Ce744d05E8ea686d9"
-// logicAddress="0x17435E20dDd57A6Ff5374B6e85CCAEfcE2422f27"
+// proxyAdminAddress="0x64A9fa6Fd332d85552E365b6877055D7C92Fa45A"
+// logicAddress="0x84A7398da3B3fc824Ab9113aF78367CFEbCFb8B5"
 
 main()
     .then(() => process.exit(0))
