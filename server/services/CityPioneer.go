060d0e883b6dcfbf5a27bf9b614383e00bbc7481
calmw <calm.wang@hotmail.com>
2023-09-09 01:14:56 +0800

    城市先锋部署

--- server/services/CityPioneer.go
+++ server/services/CityPioneer.go
@@ -12,15 +12,15 @@ func InitCityPioneer() {
 	blockchain.AdminSetMiningAddress()
 	//
 	//// 设置城市先锋考核标准
-	blockchain.AdminSetAssessmentCriteria(1, 1, 2500)
-	blockchain.AdminSetAssessmentCriteria(1, 2, 5000)
-	blockchain.AdminSetAssessmentCriteria(1, 3, 10000)
-	blockchain.AdminSetAssessmentCriteria(2, 1, 1250)
-	blockchain.AdminSetAssessmentCriteria(2, 2, 2500)
-	blockchain.AdminSetAssessmentCriteria(2, 3, 5000)
-	blockchain.AdminSetAssessmentCriteria(3, 1, 625)
-	blockchain.AdminSetAssessmentCriteria(3, 2, 125)
-	blockchain.AdminSetAssessmentCriteria(3, 3, 2500)
+	//blockchain.AdminSetAssessmentCriteria(1, 1, 2500)
+	//blockchain.AdminSetAssessmentCriteria(1, 2, 5000)
+	//blockchain.AdminSetAssessmentCriteria(1, 3, 10000)
+	//blockchain.AdminSetAssessmentCriteria(2, 1, 1250)
+	//blockchain.AdminSetAssessmentCriteria(2, 2, 2500)
+	//blockchain.AdminSetAssessmentCriteria(2, 3, 5000)
+	//blockchain.AdminSetAssessmentCriteria(3, 1, 625)
+	//blockchain.AdminSetAssessmentCriteria(3, 2, 125)
+	//blockchain.AdminSetAssessmentCriteria(3, 3, 2500)
 	//
 	//// AddCityPioneerAdmin 给城市合约、IntoMining设置用户（增加用户合约余额）添加管理员权限
 	blockchain.AddCityPioneerAdmin()
