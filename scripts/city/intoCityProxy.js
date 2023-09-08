7d58ee8ba4b815d72a99992330cb23beb8186872
calmw <calm.wang@hotmail.com>
2023-09-08 09:53:36 +0800

    用户定位主网部署

--- scripts/city/intoCityProxy.js
+++ scripts/city/intoCityProxy.js
@@ -1,21 +1,23 @@
 const {ethers, upgrades} = require("hardhat")
 require('@openzeppelin/hardhat-upgrades')
 
-// const ProxyAddr = "0x133d15CeBfaf0fb2cA91f8EA6F2c2E8B26D2B310" // 测试网
+// const ProxyAddr = "0xEe4e83f11c197B8C717E4F32902132eE08957c04" // 测试网
 const ProxyAddr = "0xebD06631510A66968f0379A4deB896d3eE7DD6ED" // 主网
 
 // 可升级合约
 async function main() {
-
-    const MyContractV2 = await ethers.getContractFactory("IntoCity");
-    // 升级
-    const contract = await upgrades.upgradeProxy(ProxyAddr, MyContractV2);
-
-    await contract.deployed();
-
-    console.log("proxy address is ", contract.address)
-    console.log("ImplementationAddress is", await upgrades.erc1967.getImplementationAddress(contract.address));
-    console.log("ProxyAdmin is", await upgrades.erc1967.getAdminAddress(contract.address));
+    // console.log("proxy address is ", contract.address)
+    console.log("ImplementationAddress is", await upgrades.erc1967.getImplementationAddress(ProxyAddr));
+    console.log("ProxyAdmin is", await upgrades.erc1967.getAdminAddress(ProxyAddr));
+    // const MyContractV2 = await ethers.getContractFactory("IntoCity");
+    // // 升级
+    // const contract = await upgrades.upgradeProxy(ProxyAddr, MyContractV2);
+    //
+    // await contract.deployed();
+    //
+    // console.log("proxy address is ", contract.address)
+    // console.log("ImplementationAddress is", await upgrades.erc1967.getImplementationAddress(contract.address));
+    // console.log("ProxyAdmin is", await upgrades.erc1967.getAdminAddress(contract.address));
 }
 
 // 测试网
