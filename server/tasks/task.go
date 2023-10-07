027e7ca223ceb4b1f9ae38a15c2784c37fe29bb6
calmw <calm.wang@hotmail.com>
2023-10-07 14:01:19 +0800

    城市先锋-上线

--- server/tasks/task.go
+++ server/tasks/task.go
@@ -3,6 +3,7 @@
 package tasks
 
 import (
+	"city-node-server/blockchain"
 	"city-node-server/db"
 	"city-node-server/services"
 	"city-node-server/tasks/service"
@@ -25,9 +26,9 @@ func Start() {
 	_ = s.Every(5).Minutes().From(gocron.NextTick()).Do(service.PollBlockTaskGetIncreaseCityDelegateEvent)
 	_ = s.Every(5).Minutes().From(gocron.NextTick()).Do(service.PollBlockTaskGetWithdrawalRewardRecordEvent)
 	_ = s.Every(30).Minutes().From(gocron.NextTick()).From(gocron.NextTick()).Do(service.GetPioneerRechargeWeight)
+	_ = s.Every(4).Hours().From(gocron.NextTick()).From(gocron.NextTick()).Do(blockchain.TriggerAllPioneerTask)
 
 	// test
-	//_ = s.Every(4).Hours().From(gocron.NextTick()).From(gocron.NextTick()).Do(blockchain.TriggerAllPioneerTask)
 	//_ = s.Every(5).Minutes().From(gocron.NextTick()).Do(service.PollBlockTaskGetUserLocationRecordEvent)
 	//_ = s.Every(3).Hours().From(gocron.NextTick()).Do(service.PollBlockTaskGetDailyRewardRecordEvent)
 	//_ = s.Every(5).Minutes().From(gocron.NextTick()).Do(service.PollBlockTaskGetRechargeRecordEvent)
