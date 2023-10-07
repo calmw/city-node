f5374e8fc5207a7d8b1b7addff44b417fd7bb030
calmw <calm.wang@hotmail.com>
2023-10-07 15:43:52 +0800

    城市先锋-上线

--- server/tasks/service/pollBlock.go
+++ server/tasks/service/pollBlock.go
@@ -8,12 +8,13 @@ import (
 
 //const PoolStep = 999
 
-const PoolStep = 1000
+const PoolStep = 500
 
 func PollBlockTaskGetUserLocationRecordEvent() {
 	Cli := blockchain.Client(blockchain.CityNodeConfig)
 	err, startBlock := blockchain.GetStartBlock(blockchain.LocationEvent) // 2306974
 	if err != nil {
+		log.Logger.Sugar().Error(err)
 		return
 	}
 	number, err := Cli.BlockNumber(context.Background())
@@ -23,23 +24,28 @@ func PollBlockTaskGetUserLocationRecordEvent() {
 	}
 	endBlock := startBlock + PoolStep
 	if endBlock > number {
+		endBlock = number
+	}
+	// 用户定位事件处理
+	err = blockchain.GetUserLocationRecordEvent(Cli, int64(startBlock), int64(endBlock))
+	if err != nil {
 		log.Logger.Sugar().Error(err)
 		return
 	}
-	// 用户定位事件处理
-	blockchain.GetUserLocationRecordEvent(Cli, int64(startBlock), int64(endBlock))
-	//err = blockchain.GetUserLocationRecordEvent(Cli, int64(startBlock), int64(endBlock))
-	//if err != nil {
-	//	return
-	//}
+
+	if endBlock >= number {
+		log.Logger.Sugar().Error("扫描区块高度>=最新高度", number)
+		return
+	}
 	// 更新区块高度
-	blockchain.SetSTartBlock(int64(startBlock+1000), blockchain.LocationEvent)
+	blockchain.SetSTartBlock(int64(endBlock), blockchain.LocationEvent)
 }
 
 func PollBlockTaskGetUserLocationRecordEvent2() {
 	Cli := blockchain.Client(blockchain.CityNodeConfig)
 	err, startBlock := blockchain.GetStartBlock(blockchain.LocationEvent2) // 2306974
 	if err != nil {
+		log.Logger.Sugar().Error(err)
 		return
 	}
 	number, err := Cli.BlockNumber(context.Background())
@@ -49,12 +55,19 @@ func PollBlockTaskGetUserLocationRecordEvent2() {
 	}
 	endBlock := startBlock + PoolStep
 	if endBlock > number {
+		endBlock = number
+	}
+	// 用户定位事件处理
+	err = blockchain.GetUserLocationRecordEvent(Cli, int64(startBlock), int64(endBlock))
+	if err != nil {
 		log.Logger.Sugar().Error(err)
 		return
 	}
-	// 用户定位事件处理
-	blockchain.GetUserLocationRecordEvent(Cli, int64(startBlock), int64(endBlock))
 
+	if endBlock >= number {
+		log.Logger.Sugar().Error("扫描区块高度>=最新高度", number)
+		return
+	}
 	// 更新区块高度
 	blockchain.SetSTartBlock(int64(startBlock+1000), blockchain.LocationEvent2)
 }
@@ -63,6 +76,7 @@ func PollBlockTaskGetDailyRewardRecordEvent() {
 	Cli := blockchain.Client(blockchain.CityNodeConfig)
 	err, startBlock := blockchain.GetStartBlock(blockchain.RewardEvent) // 2430927
 	if err != nil {
+		log.Logger.Sugar().Error(err)
 		return
 	}
 	number, err := Cli.BlockNumber(context.Background())
@@ -72,22 +86,27 @@ func PollBlockTaskGetDailyRewardRecordEvent() {
 	}
 	endBlock := startBlock + PoolStep
 	if endBlock > number {
-		log.Logger.Sugar().Error(err)
-		return
+		endBlock = number
 	}
 	err = blockchain.GetDailyRewardRecordEvent(Cli, int64(startBlock), int64(endBlock))
 	if err != nil {
 		log.Logger.Sugar().Error(err)
 		return
 	}
+
+	if endBlock >= number {
+		log.Logger.Sugar().Error("扫描区块高度>=最新高度", number)
+		return
+	}
 	// 更新区块高度
-	blockchain.SetSTartBlock(int64(startBlock+1000), blockchain.RewardEvent)
+	blockchain.SetSTartBlock(int64(endBlock), blockchain.RewardEvent)
 }
 
 func PollBlockTaskGetRechargeRecordEvent() {
 	Cli := blockchain.Client(blockchain.CityNodeConfig)
 	err, startBlock := blockchain.GetStartBlock(blockchain.RechargeEvent) // 2306974
 	if err != nil {
+		log.Logger.Sugar().Error(err)
 		return
 	}
 	number, err := Cli.BlockNumber(context.Background())
@@ -97,8 +116,7 @@ func PollBlockTaskGetRechargeRecordEvent() {
 	}
 	endBlock := startBlock + PoolStep
 	if endBlock > number {
-		log.Logger.Sugar().Error(err)
-		return
+		endBlock = number
 	}
 
 	err = blockchain.GetRechargeRecordEvent(Cli, int64(startBlock), int64(endBlock))
@@ -106,14 +124,20 @@ func PollBlockTaskGetRechargeRecordEvent() {
 		log.Logger.Sugar().Error(err)
 		return
 	}
+
+	if endBlock >= number {
+		log.Logger.Sugar().Error("扫描区块高度>=最新高度", number)
+		return
+	}
 	// 更新区块高度
-	blockchain.SetSTartBlock(int64(startBlock+1000), blockchain.RechargeEvent)
+	blockchain.SetSTartBlock(int64(endBlock), blockchain.RechargeEvent)
 }
 
 func PollBlockTaskGetIncreaseCityDelegateEvent() {
 	Cli := blockchain.Client(blockchain.CityNodeConfig)
 	err, startBlock := blockchain.GetStartBlock(blockchain.DelegateEvent) // 2306974
 	if err != nil {
+		log.Logger.Sugar().Error(err)
 		return
 	}
 	number, err := Cli.BlockNumber(context.Background())
@@ -123,8 +147,7 @@ func PollBlockTaskGetIncreaseCityDelegateEvent() {
 	}
 	endBlock := startBlock + PoolStep
 	if endBlock > number {
-		log.Logger.Sugar().Error(err)
-		return
+		endBlock = number
 	}
 
 	err = blockchain.GetIncreaseCityDelegateEvent(Cli, int64(startBlock), int64(endBlock))
@@ -132,14 +155,20 @@ func PollBlockTaskGetIncreaseCityDelegateEvent() {
 		log.Logger.Sugar().Error(err)
 		return
 	}
+
+	if endBlock >= number {
+		log.Logger.Sugar().Error("扫描区块高度>=最新高度", number)
+		return
+	}
 	// 更新区块高度
-	blockchain.SetSTartBlock(int64(startBlock+1000), blockchain.DelegateEvent)
+	blockchain.SetSTartBlock(int64(endBlock), blockchain.DelegateEvent)
 }
 
 func PollBlockTaskGetWithdrawalRewardRecordEvent() {
 	Cli := blockchain.Client(blockchain.CityNodeConfig)
 	err, startBlock := blockchain.GetStartBlock(blockchain.WithdrawEvent) // 2306974
 	if err != nil {
+		log.Logger.Sugar().Error(err)
 		return
 	}
 	number, err := Cli.BlockNumber(context.Background())
@@ -149,8 +178,7 @@ func PollBlockTaskGetWithdrawalRewardRecordEvent() {
 	}
 	endBlock := startBlock + PoolStep
 	if endBlock > number {
-		log.Logger.Sugar().Error(err)
-		return
+		endBlock = number
 	}
 
 	err = blockchain.GetWithdrawalRewardRecordEvent(Cli, int64(startBlock), int64(endBlock))
@@ -158,6 +186,11 @@ func PollBlockTaskGetWithdrawalRewardRecordEvent() {
 		log.Logger.Sugar().Error(err)
 		return
 	}
+
+	if endBlock >= number {
+		log.Logger.Sugar().Error("扫描区块高度>=最新高度", number)
+		return
+	}
 	// 更新区块高度
-	blockchain.SetSTartBlock(int64(startBlock+1000), blockchain.WithdrawEvent)
+	blockchain.SetSTartBlock(int64(endBlock), blockchain.WithdrawEvent)
 }
