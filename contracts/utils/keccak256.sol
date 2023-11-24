de006fe9acfa0b521257ba0f0c75d31f659f44a5
calmw <calm.wang@hotmail.com>
2023-11-24 21:56:23 +0800

    修改重庆用户状态

--- contracts/utils/keccak256.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-contract Keccak256 {
-    function getKeccak256(string calldata str_) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(str_));
-    }
-}
