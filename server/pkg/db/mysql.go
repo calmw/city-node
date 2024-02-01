171fcdcf0601b3d8e83c499c0f0466ac13fdb2ba
calmw <calm.wang@hotmail.com>
2024-02-01 17:09:50 +0800

    服务器迁移

--- server/pkg/db/mysql.go
+++ server/pkg/db/mysql.go
@@ -21,9 +21,9 @@ func InitMysql() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		`root`,
 		`city_node`,
-		`43.206.223.248`,
+		`18.181.188.151`,
 		`3307`,
-		`city-node`)
+		`let_me_in_gzHncU1q`)
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       dsn,   // DSN data source name
 		DefaultStringSize:         256,   // string 类型字段的默认长度
